950437738fb084b7e12432bac1f43dac6c627575
Periata <periatabreatta@gmail.com>
2018-09-22 07:33:57 +0100

    more tests for fifo, highlighting a failure

--- io881/verilog/fifo_tests.v
+++ io881/verilog/fifo_tests.v
@@ -2,8 +2,8 @@
 
 module fifo_tests;
    reg        clk;
-   reg [4:0]  enables;
-   wire [4:0] fails;
+   reg [5:0]  enables;
+   wire [5:0] fails;
    tri0       done;
    
    fifo_test_element_stores_if_next_full test0 (.clk(clk), .en(enables[0]), .fail(fails[0]), .done(done));
@@ -11,7 +11,9 @@ module fifo_tests;
    fifo_test_element_resets_if_prev_empty test2 (.clk(clk), .en(enables[2]), .fail(fails[2]), .done(done));
    fifo_test_element_copies_prev_if_full test3 (.clk(clk), .en(enables[3]), .fail(fails[3]), .done(done));
    fifo_test_int_degenerate test4 (.clk(clk), .en(enables[4]), .fail(fails[4]), .done(done));
+   fifo_test_int_2st test5 (.clk(clk), .en(enables[5]), .fail(fails[5]), .done(done));
 
+   
    always #25  // 20MHz
    //always #30 // 16.7MHz
      clk = !clk;
--- io881/verilog/tests/fifo/int_2st.v
+++ io881/verilog/tests/fifo/int_2st.v
@@ -94,6 +94,79 @@ module fifo_test_int_2st (clk, en, fail, done);
 	   $display ("fifo::int_2st - should be showing empty condition after passthrough, but was %b/%b/%b", full,empty, q_ready);
 	   fail = 1;
 	end
+
+	@(posedge clk) #1;
+	d_in = 8'h55;
+	d_in_strobe = 1;
+	
+	@(posedge clk) #15;
+	d_in_strobe = 0;
+
+	@(posedge clk) #1;
+	d_in = 8'hAA;
+	d_in_strobe = 1;
+	
+	@(posedge clk) #15;
+	d_in_strobe = 0;
+	
+	if (empty !== 0) begin
+	   $display("fifo::int_2st - should not be empty after inputx2, was %b", empty);
+	   fail = 1;
+	end
+	if (full !== 1) begin
+	   $display("fifo::int_2st - should be full after inputx2, was %b", full);
+	   fail = 1;
+	end
+	if (q != 8'h55) begin
+	   $display("fifo::int_2st - should have copied input to outputx2, was %x", q);
+	   fail = 1;
+	end
+	if (q_ready !== 1) begin
+	   $display("fifo::int_2st - should be ready after inputx2, but was %b", q_ready);
+	   fail = 1;
+	end
+
+	@(posedge clk) #1;
+	q_out_strobe = 1;
+
+	@(posedge clk) #15;
+	q_out_strobe = 0;
+
+	if (empty !== 0) begin
+	   $display("fifo::int_2st - should not be empty after inputx2/outputx1, was %b", empty);
+	   fail = 1;
+	end
+	if (full !== 0) begin
+	   $display("fifo::int_2st - should not be full after inputx2/outputx1, was %b", full);
+	   fail = 1;
+	end
+	if (q != 8'hAA) begin
+	   $display("fifo::int_2st - should have copied input#2 to outputx2/outputx1, was %x", q);
+	   fail = 1;
+	end
+	if (q_ready !== 1) begin
+	   $display("fifo::int_2st - should be ready after inputx2/outputx1, but was %b", q_ready);
+	   fail = 1;
+	end
+
+	@(posedge clk) #1;
+	q_out_strobe = 1;
+
+	@(posedge clk) #15;
+	q_out_strobe = 0;
+
+	if (empty !== 1) begin
+	   $display("fifo::int_2st - should be empty after inputx2/outputx2, was %b", empty);
+	   fail = 1;
+	end
+	if (full !== 0) begin
+	   $display("fifo::int_2st - should not be full after inputx2/outputx2, was %b", full);
+	   fail = 1;
+	end
+	if (q_ready !== 0) begin
+	   $display("fifo::int_2st - should not be ready after inputx2/outputx2, but was %b", q_ready);
+	   fail = 1;
+	end
 	
 	@(posedge clk);
 	done=1;
